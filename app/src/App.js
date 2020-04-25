2c3e638ce9b5131aae8de5bdf1c87a1802de935d
stellafang <stellafangg@gmail.com>
2020-04-25 13:46:23 -0700

    added table component

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import {Timeseries, Cards} from './components'
+import React, {useEffect, useState, useReducer} from 'react';
+import {Timeseries, Cards, CustomTable} from './components'
+import GlobalState, {reducer} from './components/global-state'
 import {fetchData} from './api'
 import styles from './app.module.css'
 
 // Use contextAPI to maintain global state for country colors?! 
 // create new component for global state Provider (use React.Fragment to wrap rest of app)
 
+const initialState = {
+  selectedCountries: null
+}
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
   const [data, setData] = useState({})
 
   const getData = async () => {
@@ -21,11 +26,14 @@ const App = () => {
   }, [])
 
   return (
-    <div className={styles.app}>
-      <h1>Covid-19 Tracker</h1>
-      <Cards totals={data.worldTotals} lastUpdated={data.lastDate}></Cards>
-      <Timeseries all={data.all} />
-    </div>
+    <GlobalState initialState={state} dispatch={dispatch}>
+      <div className={styles.app}>
+        <h1>Covid-19 Tracker</h1>
+        <Cards totals={data.worldTotals} lastUpdated={data.lastDate}></Cards>
+        <Timeseries all={data.all} />
+        <CustomTable countryTotals={data.countryTotals} />
+      </div>
+    </GlobalState>
   );
 }
 
--- app/src/components/Table/index.jsx
+++ app/src/components/Table/index.jsx
@@ -1,1 +1,103 @@
-// each row can have background color
+import React from 'react';
+import PropTypes from 'prop-types'
+import styles from './index.module.css'
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow} from '@material-ui/core'
+
+
+//TODO: background color for each row!!
+
+const columns = [
+    {id: 'country', label: 'Country', minWidth: 170},
+    {
+        id: 'totalConfirmed',
+        label: 'Confirmed',
+        minWidth: 170,
+        align: 'right',
+    },
+    {
+        id: 'totalDeaths',
+        label: 'Death',
+        minWidth: 170,
+        align: 'right'
+    },
+    {
+        id: 'totalRecovered',
+        label: 'Recovered',
+        minWidth: 170,
+        align: 'right'
+    },
+];
+
+const CustomTable = (props) => {
+    const {countryTotals} = props
+    let rows = countryTotals ? Object.entries(countryTotals)
+        .map(([country, {totalConfirmed, totalDeaths, totalRecovered}]) => (
+            {country, totalConfirmed, totalDeaths, totalRecovered}
+        )) : []
+    console.log('rowss: ', rows)
+
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+    return (
+        <Paper className={styles.root}>
+            <TableContainer className={styles.container}>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{minWidth: column.minWidth}}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.length ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                            return (
+                                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                    {columns.map((column) => {
+                                        const value = row[column.id];
+                                        return (
+                                            <TableCell key={column.id} align={column.align}>
+                                                {column.format && typeof value === 'number' ? column.format(value) : value}
+                                            </TableCell>
+                                        );
+                                    })}
+                                </TableRow>
+                            )
+                        }) : null}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+        </Paper>
+    )
+}
+
+CustomTable.propTypes = {
+    all: PropTypes.object
+}
+
+export default CustomTable
