522779fb6895f0b61c1dab3d5480a86a08c2e4a0
stellafang <stellafangg@gmail.com>
2020-04-26 00:11:43 -0700

    add country picker to timeseries

--- app/src/components/Timeseries/index.jsx
+++ app/src/components/Timeseries/index.jsx
@@ -1,31 +1,64 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
 import {Line} from 'react-chartjs-2'
 import styles from './index.module.css'
 import {getDiffInDays} from '../../api'
 
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+
 const Timeseries = (props) => {
+    const [country, setCountry] = React.useState('Canada');
     const {all, dates, dateRange} = props
-    if (!all) return '...Loading'
-    const startDateIndex = getDiffInDays(dates[0], dateRange.start)
-    const endDateIndex = getDiffInDays(dates[0], dateRange.end) + 1
-    const datesToInclude = dates.slice(startDateIndex, endDateIndex)
-    const datasets = Object.entries(all).filter(([country, stats]) => country === 'Canada')
-        .map(([country, stats]) => {
-            return {
-                data: stats.slice(startDateIndex, endDateIndex).map((stat) => stat.confirmed),
-                label: country,
-                // borderColor: 'red',
-                // fill: true
-            }
-        })
+
+    let startDateIndex
+    let endDateIndex
+    let datesToInclude
+    if (dates && dates.length > 0) {
+        startDateIndex = getDiffInDays(dates[0], dateRange.start)
+        endDateIndex = getDiffInDays(dates[0], dateRange.end) + 1
+        datesToInclude = dates.slice(startDateIndex, endDateIndex)
+    }
+
+
+    const handleChange = (event) => {
+        setCountry(event.target.value);
+    };
+
+    useEffect(() => {
+    }, [setCountry, props.dateRange])
 
     return (
         <div className={styles.timeseries}>
+            <FormControl variant="outlined" className={styles.formControl}>
+                <InputLabel id="demo-simple-select-outlined-label">Country</InputLabel>
+                <Select
+                    labelId="demo-simple-select-outlined-label"
+                    id="demo-simple-select-outlined"
+                    value={country}
+                    onChange={handleChange}
+                    label="Country"
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    {
+                        all ? Object.keys(all).map((country) => (
+                            <MenuItem value={country} key={country}>{country}</MenuItem>
+                        )) : null
+                    }
+                </Select>
+            </FormControl>
             {all ? <Line classNames
                 data={{
                     labels: datesToInclude.map((date) => date.toDateString()),
-                    datasets: datasets
+                    datasets: [{
+                        data: all[country].slice(startDateIndex, endDateIndex).map((stat) => stat.confirmed),
+                        label: country
+                    }]
                 }}
                 legend={{
                     display: false
