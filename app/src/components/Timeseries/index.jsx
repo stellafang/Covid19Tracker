833d19c4b3164ece638d29f1a55fadadeb6c6999
stellafang <stellafangg@gmail.com>
2020-04-26 21:48:40 -0700

    refactor Timeseries to use helpers

--- app/src/components/Timeseries/index.jsx
+++ app/src/components/Timeseries/index.jsx
@@ -2,10 +2,9 @@ import React, {useEffect, useContext} from 'react'
 import PropTypes from 'prop-types'
 import {Chart, Line} from 'react-chartjs-2'
 import styles from './index.module.css'
-import {getDiffInDays} from '../../api'
 import CountryPicker from '../../components/CountryPicker'
 import {GlobalStateContext} from '../global-state'
-
+import {toReadableDates, getDiffInDays, getSubsetDates} from '../../utils'
 
 const Timeseries = (props) => {
     const globalState = useContext(GlobalStateContext)
@@ -14,52 +13,13 @@ const Timeseries = (props) => {
     const {all, dateRange} = props
     let dates = props.dates
 
-    let startDateIndex
-    let endDateIndex
-    let datesToInclude
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-    if (dates && dates.length > 0) {
-        startDateIndex = getDiffInDays(dates[0], dateRange.start)
-        endDateIndex = getDiffInDays(dates[0], dateRange.end) + 1
-        dates = dates.map((date) => {
-            const month = date.getMonth()
-            const day = date.getDate()
-            return `${months[month]} ${day}`
-        })
-        datesToInclude = dates.slice(startDateIndex, endDateIndex)
-    }
-
+    let startDateIndex = dates && getDiffInDays(dates[0], dateRange.start)
+    let endDateIndex = dates && getDiffInDays(dates[0], dateRange.end) + 1
 
     const handleChange = (selectedCountries) => {
         setCountries(selectedCountries)
     };
 
-    const responsiveChartSizing = (c) => {
-        const height = c.chart.height
-        const width = c.chart.width
-        if (c.height < 800 || c.width < 600) {
-            c.scales['y-axis-0'].options.scaleLabel.fontSize = height * 5 / 200
-            c.scales['x-axis-0'].options.scaleLabel.fontSize = width * 5 / 200
-            c.scales['x-axis-0'].options.ticks.maxTicksLimit = width / 100
-            c.scales['y-axis-0'].options.ticks.maxTicksLimit = height / 50
-            c.scales['x-axis-0'].options.ticks.minor.fontSize = Math.min(width * 5 / 100, 10)
-            c.scales['y-axis-0'].options.ticks.minor.fontSize = Math.min(height * 5 / 100, 10)
-            c.titleBlock.options.fontSize = Math.min(width * 5 / 200, height * 5 / 100)
-            c.legend.options.labels.fontSize = Math.min(width * 5 / 200, height * 5 / 100)
-            c.scales['x-axis-0'].options.ticks.maxRotation = 0
-        } else {
-            c.scales['y-axis-0'].options.scaleLabel.fontSize = 25
-            c.scales['x-axis-0'].options.scaleLabel.fontSize = 25
-            c.scales['x-axis-0'].options.ticks.minor.fontSize = 10
-            c.scales['y-axis-0'].options.ticks.minor.fontSize = 10
-            c.scales['x-axis-0'].options.ticks.maxTicksLimit = 30
-            c.scales['y-axis-0'].options.ticks.maxTicksLimit = 30
-            c.scales['x-axis-0'].options.ticks.maxRotation = 30
-            c.titleBlock.options.fontSize = 28
-            c.legend.options.labels.fontSize = 20
-        }
-    }
     useEffect(() => {
         Chart.pluginService.register({
             beforeDraw: ((c, opts) => responsiveChartSizing(c))
@@ -74,9 +34,9 @@ const Timeseries = (props) => {
 
             {all ? <Line classNames
                 data={{
-                    labels: datesToInclude.map((date) => date),
+                    labels: toReadableDates(getSubsetDates(dates, dateRange.start, dateRange.end)),
                     datasets: countries.map((country) => ({
-                        data: all[country].slice(startDateIndex, endDateIndex).map((stat) => stat.confirmed),
+                        data: all[country].slice(startDateIndex, endDateIndex).map(({confirmed}) => confirmed),
                         label: country,
                         borderColor: globalState.countryToColor[country] || '#000000',
                     }))
@@ -128,4 +88,31 @@ const Timeseries = (props) => {
 Timeseries.propTypes = {
     all: PropTypes.object
 }
+
+const responsiveChartSizing = (c) => {
+    const height = c.chart.height
+    const width = c.chart.width
+    if (c.height < 800 || c.width < 600) {
+        c.scales['y-axis-0'].options.scaleLabel.fontSize = height * 5 / 200
+        c.scales['x-axis-0'].options.scaleLabel.fontSize = width * 5 / 200
+        c.scales['x-axis-0'].options.ticks.maxTicksLimit = width / 100
+        c.scales['y-axis-0'].options.ticks.maxTicksLimit = height / 50
+        c.scales['x-axis-0'].options.ticks.minor.fontSize = Math.min(width * 5 / 100, 10)
+        c.scales['y-axis-0'].options.ticks.minor.fontSize = Math.min(height * 5 / 100, 10)
+        c.titleBlock.options.fontSize = Math.min(width * 5 / 200, height * 5 / 100)
+        c.legend.options.labels.fontSize = Math.min(width * 5 / 200, height * 5 / 100)
+        c.scales['x-axis-0'].options.ticks.maxRotation = 0
+    } else {
+        c.scales['y-axis-0'].options.scaleLabel.fontSize = 25
+        c.scales['x-axis-0'].options.scaleLabel.fontSize = 25
+        c.scales['x-axis-0'].options.ticks.minor.fontSize = 10
+        c.scales['y-axis-0'].options.ticks.minor.fontSize = 10
+        c.scales['x-axis-0'].options.ticks.maxTicksLimit = 30
+        c.scales['y-axis-0'].options.ticks.maxTicksLimit = 30
+        c.scales['x-axis-0'].options.ticks.maxRotation = 30
+        c.titleBlock.options.fontSize = 25
+        c.legend.options.labels.fontSize = 15
+    }
+}
+
 export default Timeseries
