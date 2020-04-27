479e97a4c915ca6300b01b347bb242845d6f1aa9
stellafang <stellafangg@gmail.com>
2020-04-27 01:06:02 -0700

    make Table more generic

--- app/src/components/Table/index.jsx
+++ app/src/components/Table/index.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types'
 import styles from './index.module.css'
 import {Paper, Table as MaterialTable, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Grid} from '@material-ui/core'
 
+const BodyTableRow = (props) => {
+    const {row, columns, color} = props
+    const firstColId = columns && columns.length > 0 && columns[0].id
+
+    return (row && columns.length > 0 &&
+        <TableRow
+            style={{backgroundColor: color}}
+            hover
+            tabIndex={-1}
+            key={row[firstColId]}>
+            {columns.map((column, i) => <TableCell
+                key={`row-${row[firstColId]}${column.id}`}
+                align={i === 0 ? 'left' : 'right'}>
+                {row[column.id]}
+            </TableCell>
+            )}
+        </TableRow>)
+}
+
+BodyTableRow.propTypes = {
+    row: PropTypes.object.isRequired,
+    columns: PropTypes.array.isRequired,
+    color: PropTypes.string
+}
+
 const Table = (props) => {
     const {rows, columns, rowColorByFirstColumnMap} = props
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10)
+    const [page, setPage] = useState(0)
+    const [rowsPerPage, setRowsPerPage] = useState(10)
 
     return (
         <Grid className={styles.container} container item xs={12} md={9} justify='center'>
@@ -19,38 +44,17 @@ const Table = (props) => {
                                     <TableCell
                                         key={column.id}
                                         align={i === 0 ? 'left' : 'right'}
-                                        style={{minWidth: Math.max(680 / columns.length, 100)}}
-                                    >
+                                        style={{minWidth: Math.max(680 / columns.length, 100)}} >
                                         {column.label || ''}
                                     </TableCell>
                                 ))}
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {rows && rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                                return (
-                                    <TableRow
-                                        style={{
-                                            backgroundColor: rowColorByFirstColumnMap ?
-                                                rowColorByFirstColumnMap[row[columns[0].id]] : '#00000'
-                                        }}
-                                        hover
-                                        role="checkbox"
-                                        tabIndex={-1}
-                                        key={row[columns[0].id]}>
-                                        {columns.map((column, i) => {
-                                            const value = row[column.id];
-                                            return (
-                                                <TableCell
-                                                    key={`${row.code}-${column.id}=${value}`}
-                                                    align={i === 0 ? 'left' : 'right'}>
-                                                    {column.format && typeof value === 'number' ? column.format(value) : value}
-                                                </TableCell>
-                                            );
-                                        })}
-                                    </TableRow>
-                                )
-                            })}
+                            {rows && rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, i) =>
+                                <BodyTableRow key={`body-table-row-${i}`} row={row} columns={columns} color={rowColorByFirstColumnMap ?
+                                    rowColorByFirstColumnMap[row[columns[0].id]] : '#00000'} />
+                            )}
                         </TableBody>
                     </MaterialTable>
                 </TableContainer>
@@ -73,7 +77,7 @@ const Table = (props) => {
 
 Table.propTypes = {
     /**
-     * An array of rows represented by an object of keys for each column and their accompanying value.
+     * An array of rows represented by an object of keys for each column and its accompanying value.
      */
     rows: PropTypes.array.isRequired,
 
