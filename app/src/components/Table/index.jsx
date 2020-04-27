033fbb6f4a8e692c6a1f98f9d2b51b34afbc7536
stellafang <stellafangg@gmail.com>
2020-04-26 21:48:25 -0700

    make Table component standalone

--- app/src/components/Table/index.jsx
+++ app/src/components/Table/index.jsx
@@ -1,82 +1,58 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
 import styles from './index.module.css'
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Grid} from '@material-ui/core'
-import {GlobalStateContext} from '../global-state'
+import {Paper, Table as MaterialTable, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Grid} from '@material-ui/core'
 
-const columns = [
-    {id: 'country', label: 'Country', minWidth: 170},
-    {
-        id: 'totalConfirmed',
-        label: 'Confirmed',
-        minWidth: 170,
-        align: 'right',
-    },
-    {
-        id: 'totalDeaths',
-        label: 'Death',
-        minWidth: 170,
-        align: 'right'
-    },
-    {
-        id: 'totalRecovered',
-        label: 'Recovered',
-        minWidth: 170,
-        align: 'right'
-    },
-];
-
-const CustomTable = (props) => {
-    const {countryTotals} = props
+const Table = (props) => {
+    const {rows, columns, rowColorByFirstColumnMap} = props
     const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
-    const globalState = useContext(GlobalStateContext)
-
-    let rows = countryTotals || []
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
+    const [rowsPerPage, setRowsPerPage] = React.useState(10)
 
     return (
         <Grid className={styles.container} container item xs={12} md={9} justify='center'>
             <Paper className={styles.root}>
                 <TableContainer>
-                    <Table stickyHeader aria-label="sticky table">
+                    <MaterialTable stickyHeader aria-label="sticky table">
                         <TableHead>
                             <TableRow>
-                                {columns.map((column) => (
+                                {columns && columns.map((column, i) => (
                                     <TableCell
                                         key={column.id}
-                                        align={column.align}
-                                        style={{minWidth: column.minWidth}}
+                                        align={i === 0 ? 'left' : 'right'}
+                                        style={{minWidth: Math.max(680 / columns.length, 100)}}
                                     >
-                                        {column.label}
+                                        {column.label || ''}
                                     </TableCell>
                                 ))}
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {rows.length > 0 ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                            {rows && rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
                                 return (
-                                    <TableRow style={{backgroundColor: globalState.countryToColor[row['country']]}} hover role="checkbox" tabIndex={- 1} key={row['country']}>
-                                        {columns.map((column) => {
+                                    <TableRow
+                                        style={{
+                                            backgroundColor: rowColorByFirstColumnMap ?
+                                                rowColorByFirstColumnMap[row[columns[0].id]] : '#00000'
+                                        }}
+                                        hover
+                                        role="checkbox"
+                                        tabIndex={-1}
+                                        key={row[columns[0].id]}>
+                                        {columns.map((column, i) => {
                                             const value = row[column.id];
                                             return (
-                                                <TableCell key={`${row.code}-${column.id}=${value}`} align={column.align}>
+                                                <TableCell
+                                                    key={`${row.code}-${column.id}=${value}`}
+                                                    align={i === 0 ? 'left' : 'right'}>
                                                     {column.format && typeof value === 'number' ? column.format(value) : value}
                                                 </TableCell>
                                             );
                                         })}
                                     </TableRow>
                                 )
-                            }) : null}
+                            })}
                         </TableBody>
-                    </Table>
+                    </MaterialTable>
                 </TableContainer>
                 <TablePagination
                     rowsPerPageOptions={[10, 25, 100]}
@@ -84,16 +60,31 @@ const CustomTable = (props) => {
                     count={rows.length}
                     rowsPerPage={rowsPerPage}
                     page={page}
-                    onChangePage={handleChangePage}
-                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                    onChangePage={(event, newPage) => setPage(newPage)}
+                    onChangeRowsPerPage={(event) => {
+                        setPage(0)
+                        setRowsPerPage(event.target.value)
+                    }}
                 />
             </Paper>
         </Grid >
     )
 }
 
-CustomTable.propTypes = {
-    all: PropTypes.object
+Table.propTypes = {
+    /**
+     * An array of rows represented by an object of keys for each column and their accompanying value.
+     */
+    rows: PropTypes.array.isRequired,
+
+    /**
+     * List of column objects that each have an an `id` and `label`.
+     */
+    columns: PropTypes.array.isRequired,
+    /**
+     * Map of the first column value to a rgb color for that row.
+     */
+    rowColorByFirstColumnMap: PropTypes.object
 }
 
-export default CustomTable
+export default Table
