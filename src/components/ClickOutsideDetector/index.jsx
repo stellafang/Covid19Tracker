6c2c92cc23e11e7ca86145d3994e5d742efafe7b
stellafang <stellafangg@gmail.com>
2020-04-29 04:29:09 -0700

    add basic react component tests

--- src/components/ClickOutsideDetector/index.jsx
+++ src/components/ClickOutsideDetector/index.jsx
@@ -1,11 +1,11 @@
 import {useState, useEffect, useRef} from 'react'
 
 /**
- * This component allows detection of a user click outside or inside component.
+ * This component helper allows detection of a user click outside or inside component.
  * This is tailored to components you want to close when a user clicks anywhere outside the component. 
  * 
  * `useComponentVisible` takes in an boolean that represents if your component should be visible currently.
- * The component returns:
+ * This helper returns:
  * 1) a `ref` you can use to reference your component.
  * 2) `isComponentVisible` to know if a click occurred outside the component.
  * 3) `setComponentVisible` to set whether the component is visible or not.
---
+++ src/components/index.test.js
@@ -0,0 +1,38 @@
+import {mount} from 'enzyme'
+import React from 'react'
+import {ColorPicker, DateRangePicker, ItemColorPicker, ItemPicker, Table} from './index.js'
+
+describe('Basic test to check all components have no errors mounting', () => {
+    test('ColorPicker renders without errors', () => {
+        const wrapper = mount(<ColorPicker />)
+        expect(wrapper.length).toBe(1)
+    })
+    test('DateRangePicker renders without errors', () => {
+        const wrapper = mount(<DateRangePicker />)
+        expect(wrapper.length).toBe(1)
+    })
+    test('ItemColorPicker renders without errors', () => {
+        const wrapper = mount(<ItemColorPicker items={['Canada', 'China']} />)
+        expect(wrapper.length).toBe(1)
+    })
+
+    test('ItemPicker renders without errors', () => {
+        const wrapper = mount(<ItemPicker items={['Canada', 'China']} />)
+        console.log('wrapper ', wrapper)
+        expect(wrapper.length).toBe(1)
+    })
+
+    test('Table renders without errors', () => {
+        const columns = [
+            {id: 'column1', label: 'Column 1'},
+            {id: 'column2', label: 'Column 2'}
+        ]
+
+        const rows = [{column1: 'a', column2: 'aa'}, {column1: 'b', column2: 'bb'}]
+        const wrapper = mount(<Table rows={rows} columns={columns} />)
+        expect(wrapper.length).toBe(1)
+    })
+})
+
+
+
